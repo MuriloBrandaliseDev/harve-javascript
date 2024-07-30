6d576b96ea69de81f5b86da02a14c1a2da8432f8
MuriloDev <murilobrandalise98@gmail.com>
2024-07-30 19:59:07 -0300

    JS Aula-02

---
+++ aula-02/pratica-01/script.js
@@ -0,0 +1,21 @@
+function calcular (event) {
+    event.preventDefault();
+
+    //buscando o valor da nota.
+    const nota = document.getElementById("nota").value ;
+
+
+    //buscando o valor para escrever nosso resultado.
+    const resultado = document.getElementById("resultado");
+
+    if(nota < 0 || nota > 10) {
+        resultado.textContent = "Nota Inválida";
+    } else if(nota >= 7){
+        resultado.textContent = "aluno aprovado";
+    } else {
+        resultado.textContent = "aluno reprovado";
+    }
+
+
+
+}
