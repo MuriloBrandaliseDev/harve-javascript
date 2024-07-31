b4969955e4a2f0635bf5d2119f7886680729555d
MuriloDev <murilobrandalise98@gmail.com>
2024-07-30 21:07:42 -0300

    JS Aula-02 pratica 2

---
+++ aula-02/pratica-02/script.js
@@ -0,0 +1,24 @@
+function calcular(event) {
+    event.preventDefault();
+
+    //buscando o aluno
+    const aluno = document.getElementById("aluno").value;
+
+    //buscando o valor da nota.
+    const nota = Number(document.getElementById("nota").value);
+
+
+    //buscando o valor para escrever nosso resultado.
+    const resultado = document.getElementById("resultado");
+
+    if (nota < 0 || nota > 10 || !aluno || Number.isNaN(nota)) {
+        resultado.innerHTML = `<span class="reprovado">Dados inválidos</span>`;
+    } else if (nota >= 7) {
+        resultado.innerHTML = `
+        <p> O ${aluno} tirou a nota ${nota}.<span class="aprovado">Aluno APROVADO</span></p>`;
+    } else {
+        resultado.innerHTML= `
+        <p> O ${aluno} tirou a nota ${nota}. <span class="reprovado">Aluno REPROVADO</span></p>`;
+    }
+
+}
