c4be225d600b29f443c6da8218b9240f12baa6f5
MuriloDev <murilobrandalise98@gmail.com>
2024-07-30 22:25:13 -0300

    JS Aula-02 pratica 3

--- aula-02/pratica-04/script.js
+++ aula-02/pratica-04/script.js
@@ -1,7 +1,4 @@
 const calcularNota = (nota) => {
     return nota >= 6 ? "Aluno aprovado" : "Aluno reprovado";
-};
 
-// Testando a função
-let notaAluno = 7; // Você pode alterar esse valor para testar outras notas
-console.log(calcularNota(notaAluno)); // Deve imprimir "Aluno aprovado"
+}
